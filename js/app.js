bf3fc82af34719915cb66031b8ceddb3fc47a57f
José Ronaldo Souza Junior <joseronaldosjr@gmail.com>
2018-10-17 15:56:23 -0300

    Versão Básica front-end

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // var xhr = $.get("http://api.giphy.com/v1/gifs/random?q=cute+cat&api_key=YOUR_API_KEY&limit=5");
 // xhr.done(function(data) { console.log("success got data", data); });
+
+// const clickArea = document.getElementById("cat-img");
+
+// const catClicker = 
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   initApp(); // added 
+// });
