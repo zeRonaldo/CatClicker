e6a14ac43ab25f628aa7e890e15d2b9d74aeb309
José Ronaldo Souza Junior <joseronaldosjr@gmail.com>
2018-11-26 23:18:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ let catClicker = function (){
  };
 
 catClicker.prototype.loadImg = function() {
-	let api_key = '3q2HdIJkyrwyzbpPqPsVDzSOE8XL0uC0';//Substitua por sua chave de api
+	let api_key = '6d1TsNzLFm3QK3WKwsxnhYRp2NVq9Y6f';//Substitua por sua chave de api
 	let jsonObject = fetch('https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/random?tag=cute+kitten&api_key='+api_key,{
 		Method: 'GET'
 	}).then((response) => response.json())
